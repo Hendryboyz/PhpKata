c18e305eed4f6ee38e270ac1ce162b5bbf120fb4
hendryboyz <Hendry.js1247@gmail.com>
2019-03-19 09:38:31 +0800

    Kata: Roman Numberal - 2nd
     * error handling not doing

--- src/lib/Converter/RomanConverter.php
+++ src/lib/Converter/RomanConverter.php
@@ -1,55 +1,60 @@
 <?php
-declare(strict_types=1);
+declare (strict_types = 1);
 
 namespace Kata\Converter;
 
-class RomanConverter
-{
-    private $deciamlBaseNumber;
-    private $romanDecimalMap;
+class RomanConverter {
+    private $baseDecimal;
+    private $decimalRomanMap;
 
-    public function __construct()
-    {
-        $this->deciamlBaseNumber = [ 10, 5, 1 ];
-        $this->romanDecimalMap = array(
+    public function __construct() {
+        $this->baseDecimal = [1000, 500, 100, 50, 10, 5, 1];
+        $this->decimalRomanMap = array(
             1 => "I",
             5 => "V",
-            10 => "X"
+            10 => "X",
+            50 => "L",
+            100 => "C",
+            500 => "D",
+            1000 => "M"
         );
     }
 
-    public function convertFromDecimal($decimal): string
-    {
+    public function convertFromDecimal(int $decimal): string {
         $result = "";
-        foreach ($this->deciamlBaseNumber as $i => $eachBase)
-        {
-            $decrease = $this->getDecrease($eachBase, $i);
+        foreach($this->baseDecimal as $index => $eachBase) {
+            $decrease = $this->getDecrease($index);
             $decreaseNumber = $eachBase - $decrease;
-            while ($decimal >= $decreaseNumber)
-            {
-                if ($decimal < $eachBase)
-                {
+
+            while ($decreaseNumber <= $decimal) {
+                if ($decimal < $eachBase) {
+                    $result .= $this->decimalRomanMap[$decrease];
                     $decimal -= $decreaseNumber;
-                    $result .= $this->romanDecimalMap[$decrease];
                 }
-                else
-                {
+                else {
                     $decimal -= $eachBase;
                 }
-                $result .= $this->romanDecimalMap[$eachBase];
+                $result .= $this->decimalRomanMap[$eachBase];
             }
         }
         return $result;
     }
 
-    private function getDecrease($baseDecimal, $decimalIndex): int
-    {
-        $decreaseNumber = $baseDecimal >= 5 ? $this->deciamlBaseNumber[$decimalIndex + 1] : 0;
-        if ($decreaseNumber == 5)
-        {
-            $decreaseNumber = $this->deciamlBaseNumber[$decimalIndex + 2];
+    private function getDecrease(int $baseIndex): int {
+        $decrease = $this->isLastBaseDecimal($baseIndex) ? 0 : $this->baseDecimal[$baseIndex + 1];
+        if (!$this->isValidDecrease($decrease)) {
+            $decrease = $this->baseDecimal[$baseIndex + 2];
         }
-        return $decreaseNumber;
-    } 
+        return $decrease;
+    }
+
+    private function isLastBaseDecimal(int $baseIndex): bool {
+        return ($baseIndex == count($this->baseDecimal) - 1);
+    }
+
+    private function isValidDecrease(int $decrease): bool {
+        return $decrease != 5 && $decrease != 50 && $decrease != 500;
+    }
 }
+
 ?>
--- src/tests/RomanConverterTests.php
+++ src/tests/RomanConverterTests.php
@@ -1,43 +1,41 @@
 <?php
-declare(strict_types=1);
+declare (strict_types = 1);
 
 use PHPUnit\Framework\TestCase;
 use Kata\Converter\RomanConverter;
 
-class RomanConverterTests extends TestCase
-{
+class RomanConverterTests extends TestCase {
     private $converter;
 
-    public function testCanCreate(): void
-    {
+    public function testCanCreate(): void {
         $this->converter = new RomanConverter();
         $this->assertNotNull($this->converter);
     }
 
-    public function setUp(): void
-    {
+    public function setUp(): void {
         $this->testCanCreate();
     }
 
-    public function testCanConvertFromDecimal(): void
-    {
-        $roman = $this->converter->convertFromDecimal(1);
-        $this->assertEquals('I', $roman);
+    public function testCanConvertFromDecimal(): void {
+        $this->convertFromDecimalAndAssert(1, "I");
     }
 
-    /**
-     * @dataProvider lessEqualThan10Case
-     */
-    public function testGivenDecimalLessThan10_WhenConvertFromDecimal_ThemReturnRoman($decimal, $expected): void
-    {
+    private function convertFromDecimalAndAssert(int $decimal, string $expected): void {
         $roman = $this->converter->convertFromDecimal($decimal);
         $this->assertEquals($expected, $roman);
     }
 
-    public function lessEqualThan10Case(): array
-    {
+    /**
+     * @dataProvider lessThan10Case
+     */
+    public function testGivenNumberLessThan10_WhenConvertFromDecimal_ThenReturnRoman(int $decimal, string $expected): void {
+        $this->convertFromDecimalAndAssert($decimal, $expected);
+    }
+
+    public function lessThan10Case(): array {
         return array(
             array(2, "II"),
+            array(3, "III"),
             array(5, "V"),
             array(10, "X"),
             array(6, "VI"),
@@ -45,4 +43,21 @@ class RomanConverterTests extends TestCase
             array(9, "IX")
         );
     }
+
+    /**
+     * @dataProvider romanBaseDeciaml
+     */
+    public function testGivenRomanBaseDecimal_WhenConvertFromDecimal_ThenReturnRoman(int $decimal, string $expected): void {
+        $this->convertFromDecimalAndAssert($decimal, $expected);
+    }
+
+    public function romanBaseDeciaml(): array {
+        return array(
+            array(50, "L"),
+            array(100, "C"),
+            array(500, "D"),
+            array(1000, "M")
+        );
+    }
 }
+
